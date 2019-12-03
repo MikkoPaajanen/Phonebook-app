33a4eb69ceef6201e7b75a2cc9eed3a6d6c76752
MikkoPaajanen <paajasenmikko@gmail.com>
2019-12-03 17:05:50 +0200

    app to web

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const cors = require('cors')
+app.use(cors())
 
 app.use(bodyParser.json())
 
@@ -98,7 +100,7 @@ app.delete('/api/persons/:id', (req, res) => {
 
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
